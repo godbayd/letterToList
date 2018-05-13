d19cdca46ed60bd2bf047d2637ce76ca02d56fb0
Dustin Godbay <godbayd@aol.com>
2018-05-13 16:59:11 -0400

    added regex to replace whitespace with html character entity

--- src/main.js
+++ src/main.js
@@ -1,17 +1,20 @@
 function IndiLetterAnim(str, elem){
   //maybe add cache here to store already appended elems
   //so that we can program the function to only
-  //construct new Objects if an elem isnt repeated
+  //construct  new Objects if an elem isnt repeated
   //to avoid overlapping
   this.str = str;
   this.elem = document.querySelector(elem);
 }
 
 IndiLetterAnim.prototype.separateLetters = function(){
-  let arr = this.str.split('');
   //maybe some regex here to replace
   //white space and punctuation
   //with according html entity
+  let replaceWhite = this.str.replace(/\s+/g, '\u00a0');
+  augArr = replaceWhite.split(' ').toString();
+  arr = augArr.split('');
+  //regex added
   let ul = document.createElement('UL');
   ul.setAttribute('style', 'margin: 0px; padding: 0px; list-style-type: none');
 
@@ -25,6 +28,6 @@ IndiLetterAnim.prototype.separateLetters = function(){
   this.elem.appendChild(ul);
 }
 
-let indi1 = new IndiLetterAnim('test', '.letter-anim');
+let indi1 = new IndiLetterAnim('test test', '.letter-anim');
 indi1.separateLetters();
 console.log(indi1.elem.innerHTML);
