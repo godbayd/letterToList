83ded216730ed3df11c61931fb6f5bafbe7d3dc9
Dustin Godbay <godbayd@aol.com>
2018-05-13 04:23:24 -0400

    string letters successfully turned into list items and styled to read from l to r. comments left to add cache to constructor and regex to function

--- src/main.js
+++ src/main.js
@@ -1,1 +1,30 @@
+function IndiLetterAnim(str, elem){
+  //maybe add cache here to store already appended elems
+  //so that we can program the function to only
+  //construct new Objects if an elem isnt repeated
+  //to avoid overlapping
+  this.str = str;
+  this.elem = document.querySelector(elem);
+}
 
+IndiLetterAnim.prototype.separateLetters = function(){
+  let arr = this.str.split('');
+  //maybe some regex here to replace
+  //white space and punctuation
+  //with according html entity
+  let ul = document.createElement('UL');
+  ul.setAttribute('style', 'margin: 0px; padding: 0px; list-style-type: none');
+
+  arr.map((letter) => {
+    let li = document.createElement('LI');
+    li.setAttribute('style', 'float: left');
+    let strLetter = document.createTextNode(letter);
+    li.appendChild(strLetter);
+    ul.appendChild(li);
+  });
+  this.elem.appendChild(ul);
+}
+
+let indi1 = new IndiLetterAnim('test', '.letter-anim');
+indi1.separateLetters();
+console.log(indi1.elem.innerHTML);
