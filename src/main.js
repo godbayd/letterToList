09fc09807b0fd0c7c3157a67b93c9b95e30360a7
Dustin Godbay <godbayd@aol.com>
2018-05-13 19:32:25 -0400

    latest

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-function letterToList(str, elem){
+function lettersToList(str, elem){
   //maybe add cache here to store already appended elems
   //so that we can program the function to only
   //construct  new Objects if an elem isnt repeated
@@ -7,27 +7,26 @@ function letterToList(str, elem){
   this.elem = document.querySelector(elem);
 }
 
-letterToList.prototype.separateLetters = function(){
+lettersToList.prototype.separateLetters = function(){
   //maybe some regex here to replace
   //white space and punctuation
   //with according html entity
-  let replaceSpace = this.str.replace(/[\s]/g, '\u00a0');
+  const replaceSpace = this.str.replace(/[\s]/g, '\u00a0');
   augArr = replaceSpace.split(' ').toString();
   arr = augArr.split('');
   //regex added
-  let ul = document.createElement('UL');
+  const ul = document.createElement('UL');
   ul.setAttribute('style', 'margin: 0px; padding: 0px; list-style-type: none');
 
   arr.map((letter) => {
-    let li = document.createElement('LI');
+    const li = document.createElement('LI');
     li.setAttribute('style', 'float: left');
-    let strLetter = document.createTextNode(letter);
+    const strLetter = document.createTextNode(letter);
     li.appendChild(strLetter);
     ul.appendChild(li);
   });
   this.elem.appendChild(ul);
 }
 
-let indi1 = new letterToList('test test', '.letter-anim');
+const indi1 = new lettersToList('test test', '.letter-anim');
 indi1.separateLetters();
-console.log(indi1.elem.innerHTML);
